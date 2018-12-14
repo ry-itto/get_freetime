3a98067411dd937f9fd96f3f83c8e5b463135f1e
ryoya_ito <horusu.dqx@gmail.com>
2018-12-14 13:02:57 +0900

    メソッド分割，不要なコード削除

--- app/calendar_service.rb
+++ app/calendar_service.rb
@@ -14,7 +14,11 @@ class CalendarService
 
   end
 
-  def calculate_free_time(calendar_id_list, free_busy_calender)
+  ##
+  # Google Calendar APIで取得したFreeBusyを元に空き時間を取得します。
+  # @param calendar_id_list カレンダーIDのリスト
+  # @param free_busy_calender Google Calendar APIを使用して取得したFreeBusy
+  def calc_free_time(calendar_id_list, free_busy_calender)
 
     free_time = []
     previous_datetime = nil
@@ -25,25 +29,30 @@ class CalendarService
         end_jst = busy_time.end + NINE_HOUR
 
         if previous_datetime.nil?
-          previous_datetime = DateTime.new(start_jst.year,
-                                           start_jst.month,
-                                           start_jst.day,
-                                           @start_time.hour,
-                                           @start_time.min,
-                                           0,
-                                           0,
-                                           0)
+          previous_datetime = DateTime.new start_jst.year, start_jst.month,
+                                           start_jst.day, @start_time.hour
         end
 
-        interval = (start_jst.hour - previous_datetime.hour) * 60
-        interval += start_jst.min - previous_datetime.min
-
-        if interval >= @interval_time.min
+        if calc_interval(previous_datetime, start_jst) >= @interval_time.min
           free_time << { start_time: previous_datetime, end_time: start_jst }
         end
         previous_datetime = end_jst
       end
     end
     free_time
+  end
+
+  ##
+  # 二つのDateTimeを受け取り，その間時間を計算します。
+  # @param from 開始時間
+  # @param to 終了時間
+  #
+  # @return 間時間
+  def calc_interval(from, to)
+    interval = (to.hour - from.hour) * 60
+    interval += to.min - from.min
+
+    # @type Integer
+    interval
   end
 end
--- app/main.rb
+++ app/main.rb
@@ -12,6 +12,6 @@ free_busy = calendar.fetch_free_busy(calendar_id_list,
                                      DateTime.new(2018, 12, 4, 0, 0, 0, 0, 0))
 
 service = CalendarService.new
-service.calculate_free_time(calendar_id_list, free_busy).each do |free_time|
+service.calc_free_time(calendar_id_list, free_busy).each do |free_time|
   puts "start : #{free_time[:start_time]}, end : #{free_time[:end_time]}"
 end
