efce2c812cb1f5235d0768935d01ce7134c525d0
ryoya_ito <horusu.dqx@gmail.com>
2019-02-27 22:58:35 +0900

    API側へ移植できるものを移植

---
+++ api/app/services/google_calendar/calendar_fetcher.rb
@@ -0,0 +1,87 @@
+require 'google/apis/calendar_v3'
+require 'googleauth'
+require 'googleauth/stores/file_token_store'
+require 'singleton'
+
+class CalendarFetcher
+  include Singleton
+
+  APPLICATION_NAME = '空き時間出力'.freeze
+  OOB_URI = 'urn:ietf:wg:oauth:2.0:oob'.freeze
+  CREDENTIALS_PATH = '../credentials.json'.freeze
+  TOKEN_PATH = 'token.yaml'.freeze
+  SCOPE = Google::Apis::CalendarV3::AUTH_CALENDAR_READONLY
+
+  def initialize
+    @calendar_service = Google::Apis::CalendarV3::CalendarService.new
+    @calendar_service.client_options.application_name = APPLICATION_NAME
+    @calendar_service.authorization = authorize
+  end
+
+  ##
+  # API使用時に認証します。
+  #
+  # @return 認証情報
+  def authorize
+    client_id = Google::Auth::ClientId.from_file(CREDENTIALS_PATH)
+    token_store = Google::Auth::Stores::FileTokenStore.new file: TOKEN_PATH
+    authorizer = Google::Auth::UserAuthorizer.new client_id, SCOPE, token_store
+    user_id = 'default'
+    credentials = authorizer.get_credentials(user_id)
+    if credentials.nil?
+      url = authorizer.get_authorization_url base_url: OOB_URI
+      puts '以下のURLにアクセスして，ユーザでログインし，認証してください。' \
+          'そして，その結果出てきたコードを入力して ください。' + url
+      code = gets
+      credentials = authorizer.get_and_store_credentials_from_code(user_id: user_id,
+                                                                   code: code,
+                                                                   base_url: OOB_URI)
+    end
+    credentials
+  end
+
+  ##
+  # APIを使用するGoogleアカウントに紐づいているカレンダーのリストを取得します。
+  # API reference : https://developers.google.com/calendar/v3/reference/calendarList/list
+  #
+  # @return カレンダーのリスト
+  def fetch_calender_list
+    @calendar_service.list_calendar_lists
+  end
+
+  ##
+  # カレンダーIDからカレンダーに紐づくイベントのリストを取得します。
+  # API reference : https://developers.google.com/calendar/v3/reference/events/list
+  # @param calender_id カレンダーID
+  #
+  # @return イベントリスト
+  def fetch_event_list(calender_id)
+    @calendar_service.list_events calender_id,
+                                  single_events: true,
+                                  order_by: 'startTime'
+  end
+
+  ##
+  # カレンダーIDのリスト，検索日付から空き時間を取得します。
+  # API reference : https://developers.google.com/calendar/v3/reference/freebusy/query
+  #
+  # @param calendar_id_list 対象のカレンダーIDのリスト Array<String>
+  # @param time_min 検索日付(from) DateTime
+  # @param time_max 検索日付(to) DateTime
+  #
+  # @return 空き時間オブジェクト(Google::Apis::CalendarV3::FreeBusyResponse)
+  def fetch_free_busy(calendar_id_list, time_min, time_max)
+
+    # calendar_idの配列をFreeBusyRequestItemに詰め替える
+    request_items = []
+    calendar_id_list.each do |calendar_id|
+      request_items << Google::Apis::CalendarV3::FreeBusyRequestItem.new(id: calendar_id)
+    end
+
+    body = Google::Apis::CalendarV3::FreeBusyRequest.new
+    body.items = request_items
+    body.time_min = time_min
+    body.time_max = time_max
+    @calendar_service.query_freebusy body
+  end
+end
---
+++ api/app/services/google_calendar/calendar_service.rb
@@ -0,0 +1,94 @@
+require 'time'
+require 'yaml'
+
+##
+# カレンダー関係の処理を行うクラス
+class CalendarService
+
+  # 9時間
+  NINE_HOUR = Rational(3, 8)
+
+  def initialize
+    settings = YAML.load_file '../setting.yaml'
+    @start_time = Time.parse settings['start_time']
+    @end_time = Time.parse settings['end_time']
+    @interval_time = Time.parse settings['interval_time']
+    @start_date = Date.parse settings['start_date']
+    @end_date = Date.parse settings['end_date']
+
+  end
+
+  ##
+  # Google Calendar APIで取得したFreeBusyを元に空き時間を取得します。
+  # @param calendar_id_list カレンダーIDのリスト
+  def calc_free_time(calendar_id_list)
+
+    free_time = []
+    previous_datetime = nil
+
+    calendar_id_list.each do |calendar_id|
+      fetch_free_time(calendar_id_list).calendars[calendar_id].busy.each do |busy_time|
+        start_jst = busy_time.start + NINE_HOUR
+        end_jst = busy_time.end + NINE_HOUR
+
+        if previous_datetime.nil?
+          previous_datetime = DateTime.new start_jst.year, start_jst.month,
+                                           start_jst.day, @start_time.hour
+        end
+
+        if calc_interval(previous_datetime, start_jst) >= @interval_time.min
+          free_time << { start_time: previous_datetime, end_time: start_jst }
+        end
+        previous_datetime = end_jst
+      end
+    end
+
+    complete_free_days free_time
+  end
+
+  ##
+  # カレンダーIDのリストを受け取り，FreeBusyを取得します。
+  # @param calendar_id_list カレンダーIDのリスト
+  #
+  # @return Google CalendarFetcher APIを使用して取得したFreeBusy
+  def fetch_free_time(calendar_id_list)
+    calendar = CalendarFetcher.instance
+    calendar.fetch_free_busy(calendar_id_list,
+                             @start_date.to_datetime,
+                             @end_date.to_datetime)
+  end
+
+  ##
+  # 二つのDateTimeを受け取り，その間時間を計算します。
+  # @param from 開始時間
+  # @param to 終了時間
+  #
+  # @return 間時間
+  def calc_interval(from, to)
+    interval = (to.hour - from.hour) * 60
+    interval += to.min - from.min
+
+    # @type Integer
+    interval
+  end
+
+  ##
+  # 指定した日付間に丸一日空いている日があった場合に補完し，ソートします。
+  #
+  def complete_free_days(free_time)
+    target_dates = @start_date.upto(@end_date).to_a
+
+    uniq_array = free_time.map { |time| time[:start_time].to_date }.uniq
+    target_dates.each do |date|
+      next if uniq_array.include? date
+
+      start_time = DateTime.new date.year, date.month,
+                                date.day, @start_time.hour
+      end_time = DateTime.new date.year, date.month,
+                              date.day, @end_time.hour
+      free_time << { start_time: start_time, end_time: end_time }
+    end
+
+    free_time.sort_by { |time| time[:start_time] }
+  end
+end
